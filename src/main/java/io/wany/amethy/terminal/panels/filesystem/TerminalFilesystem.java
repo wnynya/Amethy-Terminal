36b8a043ccdc90710faf552d69ff609c3cb7df1e
Wany <sung@wany.io>
2023-03-01 18:27:16 +0900

    add filesystem

--- src/main/java/io/wany/amethy/terminal/panels/filesystem/TerminalFilesystem.java
+++ src/main/java/io/wany/amethy/terminal/panels/filesystem/TerminalFilesystem.java
@@ -8,7 +8,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import io.wany.amethy.terminal.Console;
 import io.wany.amethy.terminal.TerminalNode;
 import io.wany.amethy.terminal.modules.Json;
 
